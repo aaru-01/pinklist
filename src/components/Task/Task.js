4425b083e421c3107c368da38bb9cdaa7f94d705
aaru01 <aartimedhe16601@gmail.com>
2023-09-22 11:34:42 -0700

    added show list

---
+++ src/components/Task/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Task.css"
+
+export const Task = ({id, title, description, priority}) => {
+return (
+    <div className="task-container">
+        <h1 className="task-title">{title}</h1>
+<p className="task-description">{description}</p>
+<span className="task-priority">✔{priority}</span>
+    </div>
+)
+}
+export default Task
---
+++ src/views/Home/Home.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import "./Home.css"
+import Task from "./../../components/Task/Task";
+
+const Home = () => {
+    const [taskList, settaskList] = useState([
+        {
+            id: 1,
+            title: 'submit assignment',
+            description: 'Nahi to gali padegi',
+            priority: 'high'
+        },
+        {
+            id: 2,
+            title: 'Go to market',
+            description: 'Buy Alu and Gobi',
+            priority: 'Medium'
+        },
+        {
+            id: 3,
+            title: 'Build project',
+            description: 'Build project and post it on LinkedIn',
+            priority: 'very high'
+        },
+        {
+            id: 4,
+            title: 'Good Night',
+            description: 'Good night bolke so jana hai',
+            priority: 'very high'
+        }
+
+    ])
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [priority, setPriority] = useState('')
+
+const addTaskToList = () => {
+    const randomId = Math.floor(Math.random() * 1000);
+    
+    const obj = {
+        id: randomId,
+        title: title,
+        description: description,
+        priority:priority
+    }
+}
+settaskList([...taskList, obj])
+
+    return (
+        <div className="container">
+            <h1 className="app-title">PinkList 📃</h1>
+
+            <div className="todo-flex-container">
+                <div>
+                    <h2 className="text-center">Show List</h2>
+                    {
+                        taskList.map((taskItem, index) => {
+                            const { id, title, description, priority } = taskItem;
+                            return <Task id={id} title={title} description={description} priority={priority} />
+                        })
+                    }
+                </div>
+
+                <div>
+
+                    <h2 className="text-center">Add List</h2>
+                    <div className="add-task-form-container">
+                        {/* <h3>Shoe me title{title}</h3> */}
+                        <form>
+                            {/* title: */}
+                            <input type="text" 
+                            value={title} 
+                            onChange={(e) => {
+                                setTitle(e.target.value)
+                            }} 
+                            placeholder="Enter Title"
+                            className="task-input"
+                            />
+
+                            {/* description: */}
+                            <input type="text" 
+                            value={ description} 
+                            onChange={(e) => {
+                                setDescription(e.target.value)
+                            }} 
+                            placeholder="Enter Description"
+                            className="task-input"
+                            />
+
+                            {/* priority: */}
+                            <input type="text" 
+                            value={priority} 
+                            onChange={(e) => {
+                                setPriority(e.target.value)
+                            }} 
+                            placeholder="Enter Priority"
+                            className="task-input"
+                            />
+                            
+                          <button  className="btn-add-task" 
+                          type='button'
+                           onClick={addTaskToList}>
+                            Add Task to List
+                          </button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default Home 
